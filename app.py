cf17ab28c3624121878c3ef5efbd9086670b380d
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-07-30 14:21:45 -0600

    Create a view function to manage HTTP POST requests

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -14,7 +14,19 @@ stores = [
     },
 ]
 
+# All this compose a VIEW FUNCTION
 # This is a Flask endpoint
 @app.get("/store")
 def get_store():
     return {"stores" : stores}
+
+@app.post("/store")
+def create_store():
+    # Request is an object that Flask provides and contains iformation about the HTTP request
+    request_data = request.get_json()
+    new_store = {
+        "name" : request_data["name"],
+        "items" : [],
+    }
+    stores.append(new_store)
+    return new_store, 201
