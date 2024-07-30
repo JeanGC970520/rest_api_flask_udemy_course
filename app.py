ac09a9c25fc3f6335dc077a231392d7b488ef4bb
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-07-30 14:07:09 -0600

    Creating my first endpoint with Flask

--- app.py
+++ app.py
@@ -2,3 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+    {
+        "name" : "My Store",
+        "items" : [
+            {
+                "name" : "Chair",
+                "price" : 15.99,
+            },
+        ],
+    },
+]
+
+# This is a Flask endpoint
+@app.get("/store")
+def get_store():
+    return {"stores" : stores}
