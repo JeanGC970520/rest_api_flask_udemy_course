d0eebb66db32d68f76c433e9fedd11b35d619988
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-07-31 09:52:39 -0600

    Using abort method from flask-smorest to return unsuccessful responses and documenting it

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+"""
+- abort() method is using to help us document unsuccessful responses.
+"""
+
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 
 from db import items, stores
 
@@ -25,10 +30,10 @@ def create_store():
 ''
 # Dealing with URL parameters
 @app.post("/item")
-def create_item(name):
+def create_item():
     item_data = request.get_json()
     if item_data["store_id"] not in stores:
-        return {"message" : "Store not found"}, 404
+        abort(404, message="Store not found")
     item_id = uuid.uuid4().hex
     item = {**item_data, "id" : item_id}
     items[item_id] = item
@@ -43,11 +48,11 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message" : "Store not found"}, 404
+        abort(404, message="Store not found")
 
-@app.get("/item/<string:item_id>")
+@app.get("/item/<int:item_id>")
 def get_item(item_id):
     try:
         return items[item_id], 200
     except KeyError:
-        return {"message" : "Item not found"}, 404
+        abort(404, message="Item not found")
