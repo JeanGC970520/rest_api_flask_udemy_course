83231c561776c7b808678713f6d4a6cc43eb312f
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-07-31 11:36:29 -0600

    Adding new endpoint to attend DELETE or PUT request

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def create_item():
         or "name" not in item_data
     ):
         abort(400, message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload")
-    for item in item.values():
+    for item in items.values():
         if(
             item_data["name"] == item["name"]
             and item_data["store_id"] == item["store_id"]
@@ -79,3 +79,31 @@ def get_item(item_id):
         return items[item_id], 200
     except KeyError:
         abort(404, message="Item not found")
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted"}
+    except KeyError:
+        abort(404, message="Item not found")
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price' and 'name' are included in the JSON payload")
+    try:
+        item = items[item_id]
+        item |= item_data
+        return item_data
+    except KeyError:
+        abort(404, message="Item not found")
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted"}
+    except KeyError:
+        abort(404, message="Store not found")
