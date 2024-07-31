e6a7f07045726c94778a7cc4d5f67752a1e041c1
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-07-31 17:08:48 -0600

    Implementing one-to-many relationships using SQLAlchemy

--- schemas.py
+++ schemas.py
@@ -2,17 +2,23 @@ from marshmallow import Schema, fields
 
 # This is an schema, definition of the behavior in terms of input and output 
 # Helps us to validate incoming data and to convert output data according its schema
-class ItemSchema(Schema):
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
+
+class PlainStoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
 
 class ItemUpdateSchema(Schema):
     # Here one or both are optional
     name = fields.Str()
     price = fields.Float()
 
-class StoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchema(), dump_only= True))
