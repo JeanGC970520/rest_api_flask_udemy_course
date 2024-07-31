6d75bec28f47022ae280437d5be910b41da2d518
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-07-31 14:03:05 -0600

    Defining schemas to validate and convert data

---
+++ schemas.py
@@ -0,0 +1,17 @@
+from marshmallow import Schema, fields
+
+# This is an schema, definition of the behavior in terms of input and output 
+# Helps us to validate incoming data and to convert output data according its schema
+class ItemSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+    price = fields.Float(required=True)
+    store_id = fields.Str(required=True)
+
+class ItemUpdateSchema(Schema):
+    name = fields.Str()
+    price = fields.Float()
+
+class StoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
