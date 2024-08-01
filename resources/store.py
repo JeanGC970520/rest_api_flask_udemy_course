7ef042feb570cbf6bc69bc7bd844a32767f9834f
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-07-31 18:08:55 -0600

    Get models by ID from database using SQLAlchemy

--- resources/store.py
+++ resources/store.py
@@ -27,23 +27,18 @@ blp = Blueprint("stores", __name__, description="Operations on stores")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found")
+        store = StoreModel.query.get_or_404(store_id)
+        return store
 
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {"message": "Store deleted"}
-        except KeyError:
-            abort(404, message="Store not found")
+        store = StoreModel.query.get_or_404(store_id)
+        raise NotImplementedError("Deleting a store is not implemented.")
 
 @blp.route("/store")
 class StoreList(MethodView):
     @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return stores.values()
+        raise NotImplementedError("Get all stores is not implemented.")
 
     @blp.arguments(StoreSchema)
     @blp.response(201, StoreSchema)
